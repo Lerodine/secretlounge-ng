6ac4b8fd67b80a91eb6bba8f045a144dfc04e933
Pierre Biret <pierre.biret@supelec.fr>
2020-01-16 23:26:00 +0100

    edited version

--- src/replies.py
+++ src/replies.py
@@ -148,7 +148,7 @@ format_strs = {
 		"<b>{active}</b> <i>active</i>, {inactive} <i>inactive and</i> "+
 		"{blacklisted} <i>blacklisted users</i> (<i>total</i>: {total})",
 
-	types.PROGRAM_VERSION: "secretlounge-ng v{version} ~ https://github.com/sfan5/secretlounge-ng",
+	types.PROGRAM_VERSION: "secretlounge-ng v{version} ~ https://github.com/PBiret/secretlounge-ng",
 	types.HELP_MODERATOR:
 		"<i>Moderators can use the following commands</i>:\n"+
 		"  /modhelp - show this text\n"+
